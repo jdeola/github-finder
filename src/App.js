adf3b1ec1b26ec8a42b017e57f679ace39cec451
James Deola <jdeola1722@gmail.com>
2021-08-26 13:01:04 -0400

    display api data in user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,6 +12,7 @@ import axios from 'axios';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -46,9 +48,19 @@ class App extends Component {
       // note: could also have 'x' button and click event to clear
   };
 
+  // Get a single user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    this.setState({ user: res.data, loading: false });
+  };
+
+
   render() {
 
-    const {users, loading, alert} = this.state;
+    const {users, loading, alert, user} = this.state;
 
     return (
       <Router>
@@ -77,6 +89,18 @@ class App extends Component {
                   exact 
                   path='/about'
                   component={About}
+                />
+                <Route 
+                  exact 
+                  path='/user/:login'
+                  render={props => (
+                    <User 
+                    { ...props } 
+                    getUser={this.getUser} 
+                    user={user}
+                    loading={loading} 
+                    />
+                  )}
                 />
               </Switch>
 
