335053f95d3100e5aa27700eb09610aed6b17deb
James Deola <jdeola1722@gmail.com>
2021-08-26 10:11:42 -0400

    finished search API call

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
 
     state= {
         text: ''
-    }
+    };
 
-    onChange = (e) => {
-        this.setState({ text: e.target.value })
-    }
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
+    };
+    
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: '' });
+    };
+
     render() {
         return (
             <div>
-                <form action="" className="form">
+                <form onSubmit={this.onSubmit} className="form">
                     <input 
                         type="text" 
                         name='text' 
                         placeholder="Search Users.." 
                         value={this.state.text}
-                        onChange={this.onChange} />
+                        onChange={this.onChange}
+                        />
                     <input 
                         type="submit" 
                         value="Search" 
