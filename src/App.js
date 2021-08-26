e8bc1a82ede59e0798eb41e2190adbd5bc38df9c
James Deola <jdeola1722@gmail.com>
2021-08-26 10:24:13 -0400

    added clear button on search

--- src/App.js
+++ src/App.js
@@ -20,16 +20,19 @@ class App extends Component {
     client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
     this.setState({ users: res.data, loading: false });
-  }
+  };
 
   // Search Github users
   searchUsers = async text => {
     this.setState({ loading: true });
-    
+
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
     this.setState({ users: res.data.items, loading: false });
   };
+  
+  // clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
   
   render() {
     return (
@@ -37,7 +40,11 @@ class App extends Component {
         <nav className="App">
           <Navbar />
           <div className="container">
-            <Search searchUsers={this.searchUsers} />
+            <Search 
+              searchUsers={this.searchUsers} 
+              clearUsers={this.clearUsers}
+              showClear={this.state.users.length > 0 ? true: false }
+            />
             <Users loading={this.state.loading} users={this.state.users}/>
           </div>
         </nav>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,7 +8,9 @@ export class Search extends Component {
     };
 
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     };
     
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -20,6 +22,9 @@ export class Search extends Component {
     };
 
     render() {
+
+        const {showClear, clearUsers} = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
@@ -35,6 +40,14 @@ export class Search extends Component {
                         value="Search" 
                         className="btn btn-dark btn-block" />
                 </form>
+                {/* if showClear is true, button will show */}
+                {showClear && (
+                    <button 
+                        className="btn btn-light btn-block"
+                        onClick={clearUsers} >
+                    Clear
+                    </button>
+                )}
             </div>
         )
     }
