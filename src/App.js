b596ac95f4447a442b48f670081da67515c99874
James Deola <jdeola1722@gmail.com>
2021-08-26 13:22:45 -0400

    added repos to app state, user page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -57,10 +58,18 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get users Repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:ascending&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    this.setState({ repos: res.data, loading: false });
+  };
 
   render() {
 
-    const {users, loading, alert, user} = this.state;
+    const {users, user, repos, loading, alert} = this.state;
 
     return (
       <Router>
@@ -96,8 +105,10 @@ class App extends Component {
                   render={props => (
                     <User 
                     { ...props } 
-                    getUser={this.getUser} 
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos} 
                     user={user}
+                    repos={repos}
                     loading={loading} 
                     />
                   )}
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,11 @@ import React, { Fragment } from 'react';
 
 export const About = () => {
     return (
-        <fragment>
+        <Fragment>
             <h1>About This App</h1>
             <p>App to search Github users</p>
             <p>Version: 1.0.0</p>
-        </fragment>
+        </Fragment>
     )
 }
 
